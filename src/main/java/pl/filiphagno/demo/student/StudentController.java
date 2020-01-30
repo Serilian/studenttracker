adeef154e3c966bb8f9b95789b723a11c5472a52
seril <fhagno@yahoo.com>
2020-01-30 15:20:05 +0100

    Inital commit

---
+++ src/main/java/pl/filiphagno/demo/student/StudentController.java
@@ -0,0 +1,22 @@
+package pl.filiphagno.demo.student;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("api/students")
+    @CrossOrigin(origins = "*")
+    public List<Student> getAllStudents() {
+        return List.of(
+                new Student(UUID.randomUUID(), "Filip", "Hagno", "fhagno@yahoo.com", Student.Gender.MALE),
+                new Student(UUID.randomUUID(), "Monika", "Hagno", "mhagno@yahoo.com", Student.Gender.FEMALE),
+                new Student(UUID.randomUUID(), "Laurka", "Hagno", "lhagno@yahoo.com", Student.Gender.FEMALE));
+    }
+
+}
